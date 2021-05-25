881efbeaee57d0b9591596fce45a7830c2b79beb
Piotr <lugi854@gmail.com>
2021-05-25 22:24:29 +0200

    Add dbContext

---
+++ SimpleApp.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SimpleApp.Core.Models;
+
+namespace SimpleApp.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext (DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
