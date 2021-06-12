e0546f4532e6be1674bbfc7d6cf8343cb4a975cc
Piotr <lugi854@gmail.com>
2021-06-12 22:49:34 +0200

    Corrected.

--- SimpleApp.Web/ViewModels/Products/ProductViewModel.cs
+++ SimpleApp.Web/ViewModels/Products/ProductViewModel.cs
@@ -7,7 +7,7 @@ namespace SimpleApp.Web.Models
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public CategoryViewModel categoryView { get; set; }
+        public CategoryViewModel Category { get; set; }
         public decimal Price { get; set; }
     }
 }
