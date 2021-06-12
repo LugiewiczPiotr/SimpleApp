533b2a1f9bee121516c34594c01365e43339ca32
Piotr <lugi854@gmail.com>
2021-06-12 20:29:41 +0200

    Create ProductViewModel.

---
+++ SimpleApp.Web/Models/ProductViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleApp.Web.Models
+{
+    public class ProductViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public CategoryViewModel categoryView { get; set; }
+        public decimal Price { get; set; }
+    }
+}
