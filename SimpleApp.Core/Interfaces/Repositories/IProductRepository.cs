5aee618e8348489edde597de0e7006702696c08d
Piotr <lugi854@gmail.com>
2021-07-03 17:08:53 +0200

    Add generic Repository

---
+++ SimpleApp.Core/Interfaces/Repositories/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using SimpleApp.Core.Models;
+using SimpleApp.Infrastructure.Repositories;
+
+namespace SimpleApp.Core.Interfaces.Repositories
+{
+   public interface IProductRepository : IRepository<Product>
+   {
+   }
+}
