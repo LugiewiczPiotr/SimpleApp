6665b99c4e37e035dd041ac90690a0370741cbe9
Piotr <lugi854@gmail.com>
2021-07-08 22:39:19 +0200

    Create Profiles for AutoMapper.

---
+++ SimpleApp.Web/AutoMapperProfiles/CategoryProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SimpleApp.Core.Models;
+using SimpleApp.Web.Models;
+
+namespace SimpleApp.Web.AutoMapperProfiles
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<Category, CategoryViewModel>();
+        }
+    }
+}
---
+++ SimpleApp.Web/AutoMapperProfiles/ProductProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SimpleApp.Core.Models;
+using SimpleApp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleApp.Web.AutoMapperProfiles
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductViewModel>();
+        }
+    }
+}
