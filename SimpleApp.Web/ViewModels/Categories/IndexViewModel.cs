d9a0b3bb268df7b1ba931a9b71b2ad73f195310d
Piotr <lugi854@gmail.com>
2021-07-14 11:36:00 +0200

    Add IndexItemViewModel class.

--- SimpleApp.Web/ViewModels/Categories/IndexViewModel.cs
+++ SimpleApp.Web/ViewModels/Categories/IndexViewModel.cs
@@ -1,9 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
-namespace SimpleApp.Web.Models
+namespace SimpleApp.Web.ViewModels.Categories
 {
     public class IndexViewModel
     {
-        public IList<CategoryViewModel> CategoriesViewModels { get; set; }
+        public IList<IndexItemViewModel> Categories { get; set; }
+    }
+
+    public class IndexItemViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
     }
 }
