63012690e280adb11356d88934fc199eaa0e8c10
Piotr <lugi854@gmail.com>
2021-07-14 11:39:57 +0200

    fix namespace and add category id.

--- SimpleApp.Web/ViewModels/Products/ProductViewModel.cs
+++ SimpleApp.Web/ViewModels/Products/ProductViewModel.cs
@@ -1,18 +1,17 @@
-﻿using SimpleApp.Web.ViewModels;
-using System;
+﻿using System;
 using System.Collections.Generic;
 
-namespace SimpleApp.Web.Models
+namespace SimpleApp.Web.ViewModels.Products
 {
     public class ProductViewModel
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public CategoryViewModel Category { get; set; }
+       
         public decimal Price { get; set; }
 
         public IEnumerable<SelectItemViewModel> AvailableCategories { get; set; }
-        public Guid SelectedCategory { get; set; }
+        public Guid Category { get; set; }
     }
 }
