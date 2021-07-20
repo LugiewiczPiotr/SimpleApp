ede68f90f5d8b152997afef9515d85f65ecd8801
Piotr <lugi854@gmail.com>
2021-07-20 21:10:13 +0200

    load related data

--- SimpleApp.Infrastructure/Repositories/ProductRepository.cs
+++ SimpleApp.Infrastructure/Repositories/ProductRepository.cs
@@ -1,16 +1,33 @@
-﻿using SimpleApp.Core.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleApp.Core.Models;
 using SimpleApp.Infrastructure.Data;
 using SimpleApp.Infrastructure.Repositories;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace SimpleApp.Core.Interfaces.Repositories
 {
     public class ProductRepository : Repository<Product>, IProductRepository
     {
+        
         public ProductRepository(AppDbContext db) : base(db)
         {
+            
         }
+        
+
+        public override Product GetById(Guid id)
+        {
+
+            Context.Products.Include(c => c.Category).FirstOrDefault(e => e.Id == id && e.IsActive);
+            return base.GetById(id);
+        }
+                
+                
+
+
+
+           
+        
     }
 }
