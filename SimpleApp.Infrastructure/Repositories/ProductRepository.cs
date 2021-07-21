c3217f9e52d39e80e2b988e2986968d532c316fc
Piotr <lugi854@gmail.com>
2021-07-22 01:09:42 +0200

    Add a new method which removes products related to the category

--- SimpleApp.Infrastructure/Repositories/ProductRepository.cs
+++ SimpleApp.Infrastructure/Repositories/ProductRepository.cs
@@ -1,33 +1,38 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SimpleApp.Core.Interfaces.Repositories;
 using SimpleApp.Core.Models;
 using SimpleApp.Infrastructure.Data;
-using SimpleApp.Infrastructure.Repositories;
 using System;
 using System.Linq;
 
-namespace SimpleApp.Core.Interfaces.Repositories
+namespace SimpleApp.Infrastructure.Repositories
 {
     public class ProductRepository : Repository<Product>, IProductRepository
     {
-        
+
         public ProductRepository(AppDbContext db) : base(db)
         {
-            
+
         }
-        
 
         public override Product GetById(Guid id)
         {
-
-            Context.Products.Include(c => c.Category).FirstOrDefault(e => e.Id == id && e.IsActive);
+            Context.Products.Include(c => c.Category).
+                FirstOrDefault(e => e.Id == id && e.IsActive);
             return base.GetById(id);
+
         }
-                
-                
 
+        public void DeleteByCategoryId(Guid id)
+        {
+            var productCategory = Context.Products.Where(e => e.CategoryId == id).ToList();
+            foreach (var elements in productCategory)
+            {
+                elements.IsActive = false;
+            }
+            
+        }
 
-
-           
         
     }
 }
