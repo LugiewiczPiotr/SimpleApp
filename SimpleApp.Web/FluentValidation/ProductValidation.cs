6e96b2e5ede2b02e41087a0d93f8988cc5b61404
Piotr <lugi854@gmail.com>
2021-07-22 20:46:05 +0200

    Add logic validation.

---
+++ SimpleApp.Web/FluentValidation/ProductValidation.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using SimpleApp.Web.ViewModels.Products;
+
+namespace SimpleApp.Web.FluentValidation
+{
+    public class ProductValidation : AbstractValidator<ProductViewModel>
+    {
+        public ProductValidation()
+        {
+            RuleFor(x => x.Name).NotEmpty().MinimumLength(3).WithMessage("Minimum length of 5 char allowed")
+                .MaximumLength(20).WithMessage("Maximum legth of 20 char is allowed");
+            RuleFor(x => x.Description).NotEmpty().MinimumLength(5).WithMessage("Minimum length of 5 char allowed")
+                .MaximumLength(20).WithMessage("Maximum legth of 20 char is allowed");
+            RuleFor(x => x.Price).NotEmpty();
+        }
+    }
+}
