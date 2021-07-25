f02855e360be55206dda46cb3be05671be42aaaf
Piotr <lugi854@gmail.com>
2021-07-25 22:35:17 +0200

    copies the error information to ModelState

---
+++ SimpleApp.Web/ResultExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using SimpleApp.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleApp.Web
+{
+    public static class ResultExtensions
+    {
+        public static void AddErrorToModelState(this Result result, ModelStateDictionary modelState)
+        {
+            if (result.Success)
+            {
+                return;
+            }
+            foreach (var error in result.Errors)
+            {
+                modelState.AddModelError(error.PropertyName, error.Message);
+            }
+        }
+    }
+}
