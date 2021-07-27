19e32da1f3e155c81bc902f26335797b501a27c7
Piotr <lugi854@gmail.com>
2021-07-27 20:11:53 +0200

    Add message to validataion Category.

--- SimpleApp.Core/FluentValidation/ProductValidatior.cs
+++ SimpleApp.Core/FluentValidation/ProductValidatior.cs
@@ -16,11 +16,7 @@ namespace SimpleApp.Core.FluentValidation
             RuleFor(x => x.Price).NotEmpty().WithMessage("This field cannot be empty")
                 .ScalePrecision(2, 7).WithMessage(" Price must not be more than 7 digits in total," +
                 " with allowance for 2 decimals.");
-            RuleFor(x => x.Category).NotNull();
-            
-            
-
-
+            RuleFor(x => x.Category).NotNull().WithMessage("Category cannot be null");
         }
     }
 }
