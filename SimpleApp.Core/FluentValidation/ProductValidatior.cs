7ea6369217fb85ba7d2b2a1e0713e064ae1b7e7f
Piotr <lugi854@gmail.com>
2021-07-27 20:08:37 +0200

    Add Placeholders and ScalePrecision.

--- SimpleApp.Core/FluentValidation/ProductValidatior.cs
+++ SimpleApp.Core/FluentValidation/ProductValidatior.cs
@@ -8,12 +8,14 @@ namespace SimpleApp.Core.FluentValidation
         public ProductValidatior()
         {
             RuleFor(x => x.Name).NotEmpty().WithMessage("This field cannot be empty")
-                .MinimumLength(3).WithMessage("Minimum length of 5 char allowed")
-                .MaximumLength(20).WithMessage("Maximum legth of 20 char is allowed");
+                .MinimumLength(3).WithMessage("Minimum length of {MinLength} char allowed")
+                .MaximumLength(20).WithMessage("Maximum legth of {MaxLength} char is allowed");
             RuleFor(x => x.Description).NotEmpty().WithMessage("This field cannot be empty")
-                .MinimumLength(5).WithMessage("Minimum length of 5 char allowed")
-                .MaximumLength(20).WithMessage("Maximum legth of 50 char is allowed");
-            RuleFor(x => x.Price).NotEmpty().WithMessage("This field cannot be empty");
+                .MinimumLength(5).WithMessage("Minimum length of {MinLength} char allowed")
+                .MaximumLength(20).WithMessage("Maximum legth of {MaxLength} char is allowed");
+            RuleFor(x => x.Price).NotEmpty().WithMessage("This field cannot be empty")
+                .ScalePrecision(2, 7).WithMessage(" Price must not be more than 7 digits in total," +
+                " with allowance for 2 decimals.");
             RuleFor(x => x.Category).NotNull();
             
             
