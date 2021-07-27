a577c84f8f63e9312cedd12b5691ad0855bbbd8c
Piotr <lugi854@gmail.com>
2021-07-27 20:08:18 +0200

    Add Placeholders.

--- SimpleApp.Core/FluentValidation/CategoryValidator.cs
+++ SimpleApp.Core/FluentValidation/CategoryValidator.cs
@@ -8,8 +8,8 @@ namespace SimpleApp.Core.FluentValidation
         public CategoryValidator()
         {
             RuleFor(x => x.Name).NotEmpty().WithMessage("This field cannot be empty")
-                .MinimumLength(3).WithMessage("Minimum length of 3 char allowed")
-                .MaximumLength(20).WithMessage("Maximum legth of 20 char is allowed");
+                .MinimumLength(3).WithMessage("Minimum length of {MinLength} char allowed")
+                .MaximumLength(20).WithMessage("Maximum legth of {MaxLength} char is allowed");
             
         }
    
