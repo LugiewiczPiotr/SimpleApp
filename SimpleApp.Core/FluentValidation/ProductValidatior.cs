5c0d80edadf5f79140b7e75e0373da6e190cd39e
Piotr <lugi854@gmail.com>
2021-07-28 07:22:23 +0200

    Aesthetic corrections.

--- SimpleApp.Core/FluentValidation/ProductValidatior.cs
+++ SimpleApp.Core/FluentValidation/ProductValidatior.cs
@@ -12,10 +12,9 @@ namespace SimpleApp.Core.FluentValidation
                 .MaximumLength(20).WithMessage("Maximum legth of {MaxLength} char is allowed");
             RuleFor(x => x.Description).NotEmpty().WithMessage("This field cannot be empty")
                 .MinimumLength(5).WithMessage("Minimum length of {MinLength} char allowed")
-                .MaximumLength(20).WithMessage("Maximum legth of {MaxLength} char is allowed");
+                .MaximumLength(50).WithMessage("Maximum length of {MaxLength} char is allowed");
             RuleFor(x => x.Price).NotEmpty().WithMessage("This field cannot be empty")
-                .ScalePrecision(2, 7).WithMessage(" Price must not be more than 7 digits in total," +
-                " with allowance for 2 decimals.");
+                .ScalePrecision(2, 7).WithMessage(" Price must not be more than 7 digits in total,with allowance for 2 decimals.");
             RuleFor(x => x.Category).NotNull().WithMessage("Category cannot be null");
         }
     }
