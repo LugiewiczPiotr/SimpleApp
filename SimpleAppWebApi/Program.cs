4142a1c87eefcfdcc0f629c693fd0d71ee39911b
Piotr <lugi854@gmail.com>
2021-08-12 19:14:28 +0200

    change name project.

--- SimpleAppWebApi/Program.cs
+++ SimpleAppWebApi/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SimpleApp.WebApi;
 using System;
 using System.Collections.Generic;
 using System.Linq;
