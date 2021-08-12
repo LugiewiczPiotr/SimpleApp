55802b38549301b855bf662fb79d53e1736adf06
Piotr <lugi854@gmail.com>
2021-08-12 19:11:15 +0200

    Add CategoryID.

--- SimpleAppWebApi/DTO/ProductDto.cs
+++ SimpleAppWebApi/DTO/ProductDto.cs
@@ -8,6 +8,6 @@ namespace SimpleAppWebApi.DTO
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
-        public Guid Category { get; set; }
+        public Guid CategoryId { get; set; }
     }
 }
