cd96109bf6fa39dd806cca84a4527a447b2c34d8
Piotr <lugi854@gmail.com>
2022-07-16 18:41:37 +0200

    Add AutoMapper profile for user model.

---
+++ SimpleAppWebApi/AutoMapperProfiles/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using SimpleApp.Core.Models;
+using SimpleApp.WebApi.DTO;
+using AutoMapper;
+
+namespace SimpleApp.WebApi.AutoMapperProfiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, RegisterDto>()
+                .ReverseMap()
+                .ForMember(p => p.Id, opt => opt.Ignore());
+        }
+    }
+}
