8a634c303eeadce91c1647dac2888fdb2eeb7cff
Piotr <lugi854@gmail.com>
2022-07-16 18:35:52 +0200

    Add interfaces.

---
+++ SimpleApp.Core/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using SimpleApp.Core.Models;
+using SimpleApp.Infrastructure.Repositories;
+
+namespace SimpleApp.Core.Interfaces.Repositories
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        public bool emailValidate(string email);
+        public bool passwordValidate(string password);
+    }
+}
