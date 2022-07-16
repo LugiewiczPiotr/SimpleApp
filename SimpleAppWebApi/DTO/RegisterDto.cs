36920bba73bcafc822036d22bd34f013ef87ba36
Piotr <lugi854@gmail.com>
2022-07-16 18:46:34 +0200

    Add data transfer object.

---
+++ SimpleAppWebApi/DTO/RegisterDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SimpleApp.WebApi.DTO
+{
+    public class RegisterDto
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
