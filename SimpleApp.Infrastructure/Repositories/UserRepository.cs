9761934afdee7e9c7b4e7fa817db5078f0698ab2
Piotr <lugi854@gmail.com>
2022-07-16 18:40:53 +0200

    Make repository for Authenticate

---
+++ SimpleApp.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using SimpleApp.Core.Models;
+using SimpleApp.Core.Interfaces.Repositories;
+using SimpleApp.Infrastructure.Data;
+using System.Linq;
+
+namespace SimpleApp.Infrastructure.Repositories
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        public UserRepository(AppDbContext dataContext) : base(dataContext)
+        {
+
+        }
+
+        public bool emailValidate(string email)
+        {
+            return Context.Users.Any(u => u.Email == email);
+        }
+
+        public bool passwordValidate(string password)
+        {
+            return Context.Users.Any(u => u.Password == password);
+        }
+    }
+}
