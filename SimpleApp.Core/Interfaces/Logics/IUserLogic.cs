45a211d06d042a0bae9153a2bab69898b91da14c
Piotr <lugi854@gmail.com>
2022-07-18 21:49:25 +0200

    Add instance for login method.

--- SimpleApp.Core/Interfaces/Logics/IUserLogic.cs
+++ SimpleApp.Core/Interfaces/Logics/IUserLogic.cs
@@ -5,6 +5,6 @@ namespace SimpleApp.Core.Interfaces.Logics
     public interface IUserLogic : ILogic
     {
         Result<User> CreateAccount(User user);
-        Result<string> Authenticate(string login, string password);
+        Result<string> Authenticate(UserLogin userLogin);
     }
 }
