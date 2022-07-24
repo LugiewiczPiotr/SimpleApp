2634a0fa92c32291308331a4f0e6bd013834df35
Piotr <lugi854@gmail.com>
2022-07-24 22:35:30 +0200

    Add VerifyHashedPassword and code simplification.

--- SimpleApp.Core/FluentValidation/LoginValidation.cs
+++ SimpleApp.Core/FluentValidation/LoginValidation.cs
@@ -1,38 +1,30 @@
 ﻿using FluentValidation;
 using SimpleApp.Core.Interfaces.Repositories;
 using SimpleApp.Core.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace SimpleApp.Core.FluentValidation
 {
     public class LoginValidation : AbstractValidator<UserLoginAndPassword>
     {
         private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher<User> _passwordHasher;
 
-        public LoginValidation(IUserRepository userRepository)
+        public LoginValidation(IUserRepository userRepository, IPasswordHasher<User> passwordHasher)
         {
             _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
 
             RuleFor(x => x.Email).NotEmpty().WithMessage("This field cannot be empty");
-            RuleFor(x => x.Email).
-                Custom((value, context) =>
-                {
-                    var emailValidate = _userRepository.IsEmailExists(value);
-                    if (emailValidate == false)
-                    {
-                        context.AddFailure("Email or password is invalid");
-                    }
-                });
+            RuleFor(x => x.Email).Must(login => _userRepository.IsEmailExists(login))
+                .WithMessage("Email or password is invalid");
 
+            var user = _userRepository.GetAccesToDataUsers();
             RuleFor(x => x.Password).NotEmpty().WithMessage("This field cannot be empty");
-            RuleFor(x => x.Password).
-               Custom((value, context) =>
-               {
-                   var emailValidate = _userRepository.IsPasswordExists(value);
-                   if (emailValidate == false)
-                   {
-                       context.AddFailure("Email or password is invalid");
-                   }
-               });
+            RuleFor(x => x.Password).Must(
+                verifyPassword  => _passwordHasher.VerifyHashedPassword(
+                   user, user.Password , verifyPassword) == PasswordVerificationResult.Failed)
+                .WithMessage("Email or password is invalid");              
         }
     }
 }
