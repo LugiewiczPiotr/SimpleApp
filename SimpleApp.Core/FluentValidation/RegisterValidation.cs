ce2ceeda55c7f69ccde986ee69a732b9d9e28d61
Piotr <lugi854@gmail.com>
2022-07-25 20:23:13 +0200

    Add password strength validation.

--- SimpleApp.Core/FluentValidation/RegisterValidation.cs
+++ SimpleApp.Core/FluentValidation/RegisterValidation.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using SimpleApp.Core.Interfaces.Logics;
 using SimpleApp.Core.Interfaces.Repositories;
 using SimpleApp.Core.Models;
 
@@ -7,9 +8,11 @@ namespace SimpleApp.Core.FluentValidation
     public class RegisterValidation : AbstractValidator<User>
     {
         private readonly IUserRepository _userRepository;
-        public RegisterValidation(IUserRepository userRepository)
+        private readonly IAccountService _accountService;
+        public RegisterValidation(IUserRepository userRepository, IAccountService accountService)
         {
             _userRepository = userRepository;
+            _accountService = accountService;
       
             RuleFor(x => x.Email).NotEmpty().WithMessage("This field cannot be empty")
                 .EmailAddress().WithMessage(" ‘Email’ is not a valid email address.");
@@ -19,6 +22,10 @@ namespace SimpleApp.Core.FluentValidation
             RuleFor(x => x.Password).NotEmpty().WithMessage("This field cannot be empty")
                  .Length(8, 40).WithMessage
                  ("Password length should contain from {MinLength} up to {MaxLength} characters");
+
+            RuleFor(x => x.Password).Must(password => _accountService
+            .ValidatePasswordStrenght(password))
+                .WithMessage("The password should contain one uppercase letter, one lower case letter and one number");
             
 
         }
