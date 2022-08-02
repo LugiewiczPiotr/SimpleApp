9b492126402a96b9cf01d5110457f211629eaed5
Piotr <lugi854@gmail.com>
2022-08-02 20:03:09 +0200

    changing the name of the module and registering the interface

--- SimpleAppWebApi/Infrastructure/AutoFac/Modules/IdentityModule.cs
+++ SimpleAppWebApi/Infrastructure/AutoFac/Modules/IdentityModule.cs
@@ -1,15 +1,18 @@
 ﻿using Autofac;
 using Microsoft.AspNetCore.Identity;
+using SimpleApp.Core.Interfaces.Logics;
+using SimpleApp.Core.Logics;
 using SimpleApp.Core.Models;
 
 namespace SimpleApp.WebApi.Infrastructure.AutoFac.Modules
 {
-    public class PasswordHasherModule : Module
+    public class IdentityModule : Module
     {
         protected override void Load(ContainerBuilder builder)
         {
             base.Load(builder);
             builder.RegisterType<PasswordHasher<User>>().As<IPasswordHasher<User>>();
+            builder.RegisterType<AccountService>().As<IAccountService>();
         }
     }
 }
