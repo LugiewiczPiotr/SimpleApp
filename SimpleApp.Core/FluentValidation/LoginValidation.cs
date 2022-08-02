6070d9ffb5c6263933d491d7b09c664ffdbcac98
Piotr <lugi854@gmail.com>
2022-08-02 19:12:39 +0200

    Remove unnecessary property and add verfication password method.

--- SimpleApp.Core/FluentValidation/LoginValidation.cs
+++ SimpleApp.Core/FluentValidation/LoginValidation.cs
@@ -1,30 +1,24 @@
 ﻿using FluentValidation;
 using SimpleApp.Core.Interfaces.Repositories;
 using SimpleApp.Core.Models;
-using Microsoft.AspNetCore.Identity;
+using SimpleApp.Core.Interfaces.Logics;
 
 namespace SimpleApp.Core.FluentValidation
 {
     public class LoginValidation : AbstractValidator<UserLoginAndPassword>
     {
-        private readonly IUserRepository _userRepository;
-        private readonly IPasswordHasher<User> _passwordHasher;
 
-        public LoginValidation(IUserRepository userRepository, IPasswordHasher<User> passwordHasher)
-        {
-            _userRepository = userRepository;
-            _passwordHasher = passwordHasher;
+        public LoginValidation(IUserRepository userRepository, IAccountService accountService)
+        {   
 
             RuleFor(x => x.Email).NotEmpty().WithMessage("This field cannot be empty");
-            RuleFor(x => x.Email).Must(login => _userRepository.IsEmailExists(login))
+            RuleFor(x => x.Email).Must(login => userRepository.IsEmailExists(login))
                 .WithMessage("Email or password is invalid");
 
-            var user = _userRepository.GetAccesToDataUsers();
+          
             RuleFor(x => x.Password).NotEmpty().WithMessage("This field cannot be empty");
-            RuleFor(x => x.Password).Must(
-                verifyPassword  => _passwordHasher.VerifyHashedPassword(
-                   user, user.Password , verifyPassword) == PasswordVerificationResult.Failed)
-                .WithMessage("Email or password is invalid");              
+            RuleFor(x => x.Password).Must((x,y) => accountService.VerifyPassword(x)).
+                WithMessage("Email or password is invalid"); 
         }
     }
 }
