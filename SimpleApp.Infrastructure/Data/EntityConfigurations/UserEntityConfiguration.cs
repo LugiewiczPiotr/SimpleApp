f98d9d2eb646bb500063c7b791738f795070c15d
Piotr <lugi854@gmail.com>
2022-08-02 18:43:51 +0200

    Add UserEntityConfiguration.

---
+++ SimpleApp.Infrastructure/Data/EntityConfigurations/UserEntityConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SimpleApp.Core.Models;
+
+namespace SimpleApp.Infrastructure.Data.EntityConfigurations
+{
+    public class UserEntityConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(c => c.Email)
+                .IsRequired();
+
+            builder.Property(c => c.Password)
+                .IsRequired();
+        }
+    }
+}
