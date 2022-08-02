0ff3a9314d6bc069ebc41693b947c158c7bc4ec7
Piotr <lugi854@gmail.com>
2022-08-02 19:11:57 +0200

    Remove unnecessary property.

--- SimpleApp.Core/FluentValidation/RegisterValidation.cs
+++ SimpleApp.Core/FluentValidation/RegisterValidation.cs
@@ -7,25 +7,21 @@ namespace SimpleApp.Core.FluentValidation
 {
     public class RegisterValidation : AbstractValidator<User>
     {
-        private readonly IUserRepository _userRepository;
-        private readonly IAccountService _accountService;
         public RegisterValidation(IUserRepository userRepository, IAccountService accountService)
         {
-            _userRepository = userRepository;
-            _accountService = accountService;
-      
+
             RuleFor(x => x.Email).NotEmpty().WithMessage("This field cannot be empty")
                 .EmailAddress().WithMessage(" ‘Email’ is not a valid email address.");
 
-            RuleFor(x => x.Email).Must(login => !_userRepository.IsEmailExists(login))
+            RuleFor(x => x.Email).Must(login => !userRepository.IsEmailExists(login))
                 .WithMessage("That email is taken");
 
             RuleFor(x => x.Password).NotEmpty().WithMessage("This field cannot be empty")
                  .Length(8, 40).WithMessage
                  ("Password length should contain from {MinLength} up to {MaxLength} characters");
 
-            RuleFor(x => x.Password).Must(password => _accountService
-            .ValidatePasswordStrenght(password))
+            RuleFor(x => x.Password).Must(password => accountService
+            .ValidatePasswordStrength(password))
                 .WithMessage("The password should contain one uppercase letter, one lower case letter and one number");
             
 
