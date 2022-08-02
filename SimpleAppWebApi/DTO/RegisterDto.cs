3e022107c28c1dd860ca0e40d876ab1679d3e7db
Piotr <lugi854@gmail.com>
2022-08-02 18:41:35 +0200

    Remove unessery property

--- SimpleAppWebApi/DTO/RegisterDto.cs
+++ SimpleAppWebApi/DTO/RegisterDto.cs
@@ -4,7 +4,6 @@ namespace SimpleApp.WebApi.DTO
 {
     public class RegisterDto
     {
-        public Guid Id { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
     }
