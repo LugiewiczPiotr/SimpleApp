f029546054b9cf9e71d2252ed3f23ba1d961c2c0
Piotr <lugi854@gmail.com>
2022-09-04 21:38:33 +0200

    Add Order and OrderItem etnites.

--- SimpleApp.Infrastructure/Data/AppDbContext.cs
+++ SimpleApp.Infrastructure/Data/AppDbContext.cs
@@ -16,6 +16,8 @@ namespace SimpleApp.Infrastructure.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
