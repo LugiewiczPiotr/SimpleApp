57e748685264c3f9f00de0c4ad535aae69d159b0
Piotr <lugi854@gmail.com>
2022-09-04 21:39:08 +0200

    Add many to many relation.

---
+++ SimpleApp.Core/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SimpleApp.Core.Models
+{
+    public class Order : BaseModel
+    {
+       public Guid UserId { get; set; }
+       public User User { get; set; }
+    }
+}
---
+++ SimpleApp.Core/Models/OrderItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SimpleApp.Core.Models
+{
+    public class OrderItem : BaseModel
+    {
+        public int Quantity { get; set; }
+        public Guid OrderId { get; set; }
+        public Order Order { get; set; }
+        public Guid ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
