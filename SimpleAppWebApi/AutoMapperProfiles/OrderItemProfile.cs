f1affaa1346ebaf47f32a897a8a42eff79b046eb
Piotr <lugi854@gmail.com>
2022-09-26 22:10:27 +0200

    adding maper profiles.

---
+++ SimpleAppWebApi/AutoMapperProfiles/OrderItemProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using SimpleApp.Core.Models;
+using SimpleApp.WebApi.DTO;
+
+namespace SimpleApp.WebApi.AutoMapperProfiles
+{
+    public class OrderItemProfile : Profile
+    {
+        public OrderItemProfile()
+        {
+            CreateMap<OrderItemDto, OrderItem>()
+                .ReverseMap();
+        }
+    }
+}
---
+++ SimpleAppWebApi/AutoMapperProfiles/OrderProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using SimpleApp.Core.Models;
+using SimpleApp.WebApi.DTO;
+
+namespace SimpleApp.WebApi.AutoMapperProfiles
+{
+    public class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<Order, OrderDto>()
+                .ReverseMap();
+        }
+    }
+}
