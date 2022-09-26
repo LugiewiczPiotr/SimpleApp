3e7ba151700af07e0e00e2098691c746b43394cc
Piotr <lugi854@gmail.com>
2022-09-26 22:14:14 +0200

    Make extension method for get current user who use API.

---
+++ SimpleAppWebApi/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Claims;
+
+namespace SimpleApp.WebApi
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static Guid GetUserId(this ClaimsPrincipal claimsPrincipal)
+        {
+            var user = claimsPrincipal.FindFirst(ClaimTypes.NameIdentifier);
+
+            return new Guid(user.Value);
+        }
+
+    }
+}
