136b503f9832855b90fd75b5f132040aeb2cddcc
Piotr <lugi854@gmail.com>
2022-10-11 21:14:38 +0200

    remove blank space.

--- SimpleApp.Core/FluentValidation/RegisterValidatior.cs
+++ SimpleApp.Core/FluentValidation/RegisterValidatior.cs
@@ -11,16 +11,13 @@ namespace SimpleApp.Core.FluentValidation
         {
             RuleFor(x => x.Email).NotEmpty().WithMessage("This field cannot be empty")
                 .EmailAddress().WithMessage(" ‘Email’ is not a valid email address.")
-
                 .Must(login => !userRepository.CheckIfUserExists(login))
                 .WithMessage("That email is taken");
 
             RuleFor(x => x.Password).NotEmpty().WithMessage("This field cannot be empty")
                  .Length(8, 40).WithMessage("Password length should contain from " +
                  "{MinLength} up to {MaxLength} characters")
-
                  .Must(password => accountService
-
                  .ValidatePasswordStrength(password))
                  .WithMessage("The password should contain one uppercase letter," +
                  " one lower case letter and one number");
