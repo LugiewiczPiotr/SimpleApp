e415f17835d2bc76ee976a4f17a4cd56554cf678
Piotr <lugi854@gmail.com>
2022-10-11 21:22:35 +0200

    correct name.

--- SimpleApp.Core/FluentValidation/CategoryValidator.cs
+++ SimpleApp.Core/FluentValidation/CategoryValidator.cs
@@ -9,7 +9,7 @@ namespace SimpleApp.Core.FluentValidation
         {
             RuleFor(x => x.Name).NotEmpty().WithMessage("This field cannot be empty")
                 .MinimumLength(3).WithMessage("Minimum length of {MinLength} char allowed")
-                .MaximumLength(20).WithMessage("Maximum legth of {MaxLength} char is allowed");
+                .MaximumLength(20).WithMessage("Maximum length of {MaxLength} char is allowed");
         }
     }
 }
