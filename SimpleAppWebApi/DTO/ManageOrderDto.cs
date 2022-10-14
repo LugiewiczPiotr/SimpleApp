f83e95df5c65e540c3b2844ec140a89a3c291fc5
Piotr <lugi854@gmail.com>
2022-10-14 20:46:10 +0200

    change type.

--- SimpleAppWebApi/DTO/ManageOrderDto.cs
+++ SimpleAppWebApi/DTO/ManageOrderDto.cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections.Generic;
-using SimpleApp.Core.Models;
 
 namespace SimpleApp.WebApi.DTO
 {
     public class ManageOrderDto
     {
         public ICollection<OrderItemDto> OrderItems { get; set; }
-        public StatusOrder Status { get; set; }
+        public string Status { get; set; }
     }
 }
