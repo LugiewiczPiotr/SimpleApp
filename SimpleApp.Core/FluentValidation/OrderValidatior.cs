eb012510db964bc2e1119a035f2905f818ec63f6
Piotr <lugi854@gmail.com>
2022-10-30 18:02:44 +0100

    change name class.

--- SimpleApp.Core/FluentValidation/OrderValidatior.cs
+++ SimpleApp.Core/FluentValidation/OrderValidatior.cs
@@ -4,18 +4,20 @@ using SimpleApp.Core.Models;
 
 namespace SimpleApp.Core.FluentValidation
 {
-    public class OrderValidation : AbstractValidator<Order>
+    public class OrderValidatior : AbstractValidator<Order>
     {
-        public OrderValidation(IProductRepository productRepository)
+        public OrderValidatior(IProductRepository productRepository)
         {
             RuleForEach(x => x.OrderItems).ChildRules(orders =>
             {
-                orders.RuleFor(x => x.ProductId).NotEmpty()
+                orders.RuleFor(x => x.ProductId)
+                .NotEmpty()
                 .WithMessage("This field cannot be empty")
                 .Must(product => productRepository.CheckIfProductExist(product))
                 .WithMessage("Product dont exist");
 
-                orders.RuleFor(x => x.Quantity).NotEmpty()
+                orders.RuleFor(x => x.Quantity)
+                .NotEmpty()
                 .WithMessage("This field cannot be empty")
                 .GreaterThan(0).WithMessage("Value must be greater than 0");
             });
