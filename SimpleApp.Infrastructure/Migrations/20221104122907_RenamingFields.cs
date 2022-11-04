cb9642fd7a4a018c7267138b7f225514fd3c9d1e
Piotr <lugi854@gmail.com>
2022-11-04 13:59:08 +0100

    add new migration.

---
+++ SimpleApp.Infrastructure/Migrations/20221104122907_RenamingFields.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SimpleApp.Infrastructure.Migrations
+{
+    public partial class RenamingFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CancelledOn",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "FinalizedOn",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "PlacedOn",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CancelledAt",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FinalizedAt",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PlacedAt",
+                table: "Orders",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CancelledAt",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "FinalizedAt",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "PlacedAt",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CancelledOn",
+                table: "Orders",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FinalizedOn",
+                table: "Orders",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PlacedOn",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
