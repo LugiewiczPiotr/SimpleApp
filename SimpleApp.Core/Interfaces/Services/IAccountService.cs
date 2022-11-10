54f70159012e449104616964718ee2b6b8c4117f
Piotr <lugi854@gmail.com>
2022-11-10 18:19:02 +0100

    Add new folder "Services"

--- SimpleApp.Core/Interfaces/Services/IAccountService.cs
+++ SimpleApp.Core/Interfaces/Services/IAccountService.cs
@@ -1,6 +1,7 @@
-﻿using SimpleApp.Core.Models.Entity;
+﻿using SimpleApp.Core.Models;
+using SimpleApp.Core.Models.Entities;
 
-namespace SimpleApp.Core.Interfaces.Logics
+namespace SimpleApp.Core.Interfaces.Services
 {
     public interface IAccountService
     {
