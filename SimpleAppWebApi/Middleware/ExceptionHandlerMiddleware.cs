ab2ed27c5edb58031541b928a74fc666c9398e65
Piotr <lugi854@gmail.com>
2022-11-13 20:42:18 +0100

    Add ExceptionHandlerMiddleware.

---
+++ SimpleAppWebApi/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Serilog;
+
+namespace SimpleApp.WebApi.Middleware
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next, ILogger logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception error)
+            {
+                _logger.Error(error, "An unhandled exception has occurred");
+            }
+        }
+    }
+}
