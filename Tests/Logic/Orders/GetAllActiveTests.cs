a5687dc39f8e29ec9006a3d3fcb92b93f680fd5e
Piotr <lugi854@gmail.com>
2022-12-31 15:59:26 +0100

    remove unessery using.

--- Tests/Logic/Orders/GetAllActiveTests.cs
+++ Tests/Logic/Orders/GetAllActiveTests.cs
@@ -3,7 +3,6 @@ using System.Threading.Tasks;
 using FizzWare.NBuilder;
 using Moq;
 using SimpleApp.Core.Models.Entities;
-using System.Linq;
 using Xunit;
 
 namespace SimpleApp.Core.UnitTests.Logic.Orders
