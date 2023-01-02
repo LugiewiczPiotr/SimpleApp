3b7e724ffeb66f42386b6565d2374072c861d97b
Piotr <lugi854@gmail.com>
2023-01-02 22:43:10 +0100

    add migration for optimistic concurrency.

---
+++ SimpleApp.Infrastructure/Migrations/20230102213023_Optimistic_concurrency_control.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimpleApp.Infrastructure.Migrations
+{
+    public partial class Optimistic_concurrency_control : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "Products",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "Orders",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "OrderItems",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "Categories",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "Categories");
+        }
+    }
+}
